cc62107aeb0cb7ac930a185f5133ab2f00468c8e
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-01-10 02:20:39 +0100

    mini mega menu

---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>mega menu</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <header>
+        <nav>
+            <ul class="level0">
+                <li><a href="">Home</a>
+                    <ul class="level1">
+                        <li><a href="">Look</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="">Love</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="">Like</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="">Essentiel01</a>
+                    <ul class="level1">
+                        <li><a href="">Look</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="">Love</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="">Like</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="">Flyerfly</a>
+                    <ul class="level1">
+                        <li><a href="">Look</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="">Love</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="">Like</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="">Clétus</a>
+                    <ul class="level1">
+                        <li><a href="">Look</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="">Love</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="">Like</a>
+                            <ul class="level2">
+                                <li><a href="">Look</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Like</a>
+                                    <ul class="level3">
+                                        <li><a href="">Last</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    </body>
+</html>
